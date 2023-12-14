7ca47aa724720383f9d31d2507c516621bada0c7
Ruan Moras <ruanmoraes159@gmail.com>
2023-12-14 11:45:44 -0300

    update 14/12/2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,7 @@ AOS.init()
 
 const menu = document
     .querySelector('.header__menu')
-    .addEventListener('click', openMenu)
-
-function openMenu() {
-    const showMenu = document.querySelector('.header__mobile')
-    showMenu.classList.toggle('is-active')
-}
+    .addEventListener('click', () => {
+        const showMenu = document.querySelector('.header__links')
+        showMenu.classList.toggle('menuIsActivated')
+    })
