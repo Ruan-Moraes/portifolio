a36dbfc479e1b9f580dfd8be22e90687f7c77a4f
Ruan Moras <ruanmoraes159@gmail.com>
2023-12-18 12:36:33 -0300

    update 18/12/2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,17 @@ const menu = document
         const showMenu = document.querySelector('.header__links')
         showMenu.classList.toggle('menuIsActivated')
     })
+
+// Settings
+
+const settings = document
+    .querySelector('.settings')
+    .addEventListener('click', () => {
+        const gear = document.querySelector('.settings > .fa-gear')
+        gear.style.transform = 'rotate(360deg)'
+        openSettings(gear)
+    })
+
+function openSettings(gear) {
+    const gears = gear
+}
